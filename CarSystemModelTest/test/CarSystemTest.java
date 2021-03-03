c43a303b5fdcda0d013d2afb8c5189e75a353637
bernardperru <47976460+bernardperru@users.noreply.github.com>
2021-03-03 13:53:28 +0100

    Egoneretnæsehorn

--- CarSystemModelTest/test/CarSystemTest.java
+++ CarSystemModelTest/test/CarSystemTest.java
@@ -14,42 +14,50 @@ void test(){
 CarSystem cs = new CarSystem();
 
 cs.close();
+
 assertFalse(cs.locked);
-assertTrue(cs.unlocked);
-assertFalse(cs.opened);
 assertTrue(cs.closed);
 assertFalse(cs.armed);
 
 cs.lock();
 cs.c = 0;
+
 assertTrue(cs.c == 0);
 assertTrue(cs.locked);
 assertTrue(cs.closed);
-assertFalse(cs.opened);
-assertFalse(cs.unlocked);
+
 cs.tick(20);
 cs.armedOn();
+
 assertTrue(cs.armed);
 
 cs.open();
 cs.e = 0;
+
 assertTrue(cs.e <= 0);
-assertTrue(cs.opened);
+assertFalse(cs.closed);
+
 cs.armedOff();
+
 assertTrue(cs.e <= 0);
 assertFalse(cs.armed);
+
 cs.flashOn();
+
 assertTrue(cs.e <= 0);
 assertTrue(cs.flash);
+
 cs.soundOn();
 assertTrue(cs.sound);
+
 assertTrue(cs.e<=30);
 assertTrue(cs.sound);
 
 assertTrue(cs.e < 30);
 cs.g = 0;
 cs.unlock();
+
 assertTrue(cs.g <= 0);
-assertTrue(cs.unlocked);
+assertFalse(cs.locked);
 }
 }
