8e036739d4515e03ae0416d7ddd789756e61b14e
bernardperru <47976460+bernardperru@users.noreply.github.com>
2021-03-03 13:28:15 +0100

    update

--- CarSystemModelTest/src/CarSystem.java
+++ CarSystemModelTest/src/CarSystem.java
@@ -1,12 +1,7 @@
-
-
-
 public class CarSystem implements ICarAlarmSystem {
 
-    boolean opened = true;
     boolean closed = false;
     boolean locked = false;
-    boolean unlocked = true;
     boolean flash = false;
     boolean sound = false;
     boolean armed = false;
@@ -21,24 +16,20 @@ public class CarSystem implements ICarAlarmSystem {
     @Override
     public void lock() {
         locked = true;
-        unlocked = false;
     }
 
     @Override
     public void unlock() {
-        unlocked = true;
         locked = false;
     }
 
     @Override
     public void close() {
         closed = true;
-        opened = false;
     }
 
     @Override
     public void open() {
-        opened = true;
         closed = false;
     }
 
